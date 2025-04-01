d6b8b2efabf19dac66127d1fca396d9600dd3875
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-03-31 21:04:10 -0300

    add: primary input component

--- login/src/app/pages/login/login.component.ts
+++ login/src/app/pages/login/login.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
 
 @Component({
   selector: 'app-login',
-  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule],
+  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup<LoginForm>;
 
+  constructor(
+    private router: Router,
+  ){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
