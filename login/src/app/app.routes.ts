a6987a8fba9b6419397c3895b1ecac66a12ef943
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-04-15 21:14:56 -0300

    integrated with backend

--- login/src/app/app.routes.ts
+++ login/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,10 @@ export const routes: Routes = [
     {
         path: "signup",
         component: SignupComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: []
     }
 ];
